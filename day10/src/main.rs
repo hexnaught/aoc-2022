ca664ab393b625c053eccdb6c46c2476159c47e7
Dan <danj.parkes@gmail.com>
2022-12-10 16:11:28 +0000

    feat: add working day 10

---
+++ day10/src/main.rs
@@ -0,0 +1,486 @@
+use common;
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum OP {
+    NOOP,
+    Addx(i32),
+}
+
+#[derive(Debug)]
+struct InvalidOpError;
+impl FromStr for OP {
+    type Err = InvalidOpError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let split_result = s.split_once(" ");
+
+        return match split_result {
+            None => match s {
+                "noop" => Ok(OP::NOOP),
+                &_ => Err(InvalidOpError),
+            },
+            Some((operation, value)) => match operation {
+                "addx" => Ok(OP::Addx(value.parse().unwrap_or(0))),
+                &_ => Err(InvalidOpError),
+            },
+        };
+    }
+}
+
+struct Program {
+    pc: i32,
+    cycle_pc: i32,
+    x_reg: i32,
+    signal_strength: i32,
+    pixel_buffer: Vec<char>,
+}
+
+impl Program {
+    pub fn new() -> Self {
+        Program {
+            pc: 1,
+            cycle_pc: 0,
+            x_reg: 1,
+            signal_strength: 0,
+            pixel_buffer: vec![],
+        }
+    }
+
+    pub fn execute_op(&mut self, op: OP) -> i32 {
+        match op {
+            OP::NOOP => self.noop(),
+            OP::Addx(v) => self.addx(v),
+        }
+        0
+    }
+
+    fn noop(&mut self) {
+        self.draw_pixel();
+        self.increment_pc();
+    }
+
+    fn addx(&mut self, amount: i32) {
+        self.draw_pixel();
+        self.increment_pc();
+        self.draw_pixel();
+        self.x_reg = self.x_reg + amount;
+        self.increment_pc();
+    }
+
+    fn increment_pc(&mut self) {
+        self.pc = self.pc + 1;
+        self.cycle_pc = self.cycle_pc + 1;
+
+        if self.cycle_pc >= 40 {
+            self.cycle_pc = 0;
+        }
+
+        self.calc_signal_strength();
+    }
+
+    fn calc_signal_strength(&mut self) {
+        match self.pc {
+            20 | 60 | 100 | 140 | 180 | 220 => {
+                self.signal_strength = self.signal_strength + (self.pc * self.x_reg);
+            }
+            _ => (),
+        }
+    }
+
+    fn draw_pixel(&mut self) {
+        print!(
+            " // PC: {:?} : CyclePC: {:?} | Sprite: [{:?} {:?} {:?}]",
+            self.pc,
+            self.cycle_pc,
+            self.x_reg - 1,
+            self.x_reg,
+            self.x_reg + 1
+        );
+
+        if self.cycle_pc >= self.x_reg - 1 && self.cycle_pc <= self.x_reg + 1 {
+            self.pixel_buffer.push('#')
+        } else {
+            self.pixel_buffer.push('.')
+        }
+    }
+}
+
+fn main() {
+    let input_path = common::get_filepath_arg().unwrap();
+    let input_vec = match common::read_file_lines::<String>(input_path.as_str()) {
+        Ok(contents) => contents,
+        Err(error) => panic!("Problem reading the file contents: {:?}", error),
+    };
+
+    let p1_res = part_one(&input_vec);
+    println!("Part 1: {:?}", p1_res);
+
+    let p2_res = part_two(&input_vec);
+    println!("");
+    for row in p2_res.chunks(40) {
+        println!("{}", row.iter().collect::<String>())
+    }
+}
+
+fn part_one(input: &Vec<String>) -> i32 {
+    let mut program = Program::new();
+
+    for instruction in input.iter() {
+        let op = OP::from_str(&instruction).expect("invalid opcode/instruction");
+
+        println!(
+            "PC: {} | X: {} | SIG: {} // OP: {:?}",
+            program.pc, program.x_reg, program.signal_strength, op,
+        );
+
+        program.execute_op(op);
+    }
+
+    program.signal_strength
+}
+
+fn part_two(input: &Vec<String>) -> Vec<char> {
+    let mut program = Program::new();
+
+    for instruction in input.iter() {
+        let op = OP::from_str(&instruction).expect("invalid opcode/instruction");
+        program.execute_op(op);
+    }
+
+    program.pixel_buffer
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{part_one, part_two};
+
+    #[test]
+    fn example_part_one() {
+        let input: Vec<String> = vec![
+            "addx 15".to_string(),
+            "addx -11".to_string(),
+            "addx 6".to_string(),
+            "addx -3".to_string(),
+            "addx 5".to_string(),
+            "addx -1".to_string(),
+            "addx -8".to_string(),
+            "addx 13".to_string(),
+            "addx 4".to_string(),
+            "noop".to_string(),
+            "addx -1".to_string(),
+            "addx 5".to_string(),
+            "addx -1".to_string(),
+            "addx 5".to_string(),
+            "addx -1".to_string(),
+            "addx 5".to_string(),
+            "addx -1".to_string(),
+            "addx 5".to_string(),
+            "addx -1".to_string(),
+            "addx -35".to_string(),
+            "addx 1".to_string(),
+            "addx 24".to_string(),
+            "addx -19".to_string(),
+            "addx 1".to_string(),
+            "addx 16".to_string(),
+            "addx -11".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 21".to_string(),
+            "addx -15".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx -3".to_string(),
+            "addx 9".to_string(),
+            "addx 1".to_string(),
+            "addx -3".to_string(),
+            "addx 8".to_string(),
+            "addx 1".to_string(),
+            "addx 5".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx -36".to_string(),
+            "noop".to_string(),
+            "addx 1".to_string(),
+            "addx 7".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 2".to_string(),
+            "addx 6".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 7".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "addx -13".to_string(),
+            "addx 13".to_string(),
+            "addx 7".to_string(),
+            "noop".to_string(),
+            "addx 1".to_string(),
+            "addx -33".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 2".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 8".to_string(),
+            "noop".to_string(),
+            "addx -1".to_string(),
+            "addx 2".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "addx 17".to_string(),
+            "addx -9".to_string(),
+            "addx 1".to_string(),
+            "addx 1".to_string(),
+            "addx -3".to_string(),
+            "addx 11".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx -13".to_string(),
+            "addx -19".to_string(),
+            "addx 1".to_string(),
+            "addx 3".to_string(),
+            "addx 26".to_string(),
+            "addx -30".to_string(),
+            "addx 12".to_string(),
+            "addx -1".to_string(),
+            "addx 3".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx -9".to_string(),
+            "addx 18".to_string(),
+            "addx 1".to_string(),
+            "addx 2".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 9".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx -1".to_string(),
+            "addx 2".to_string(),
+            "addx -37".to_string(),
+            "addx 1".to_string(),
+            "addx 3".to_string(),
+            "noop".to_string(),
+            "addx 15".to_string(),
+            "addx -21".to_string(),
+            "addx 22".to_string(),
+            "addx -6".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "addx 2".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "addx -10".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 20".to_string(),
+            "addx 1".to_string(),
+            "addx 2".to_string(),
+            "addx 2".to_string(),
+            "addx -6".to_string(),
+            "addx -11".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+        ];
+        let expected = 13140;
+        let result = part_one(&input);
+        assert_eq!(expected, result)
+    }
+
+    #[test]
+    fn example_part_two() {
+        let input: Vec<String> = vec![
+            "addx 15".to_string(),
+            "addx -11".to_string(),
+            "addx 6".to_string(),
+            "addx -3".to_string(),
+            "addx 5".to_string(),
+            "addx -1".to_string(),
+            "addx -8".to_string(),
+            "addx 13".to_string(),
+            "addx 4".to_string(),
+            "noop".to_string(),
+            "addx -1".to_string(),
+            "addx 5".to_string(),
+            "addx -1".to_string(),
+            "addx 5".to_string(),
+            "addx -1".to_string(),
+            "addx 5".to_string(),
+            "addx -1".to_string(),
+            "addx 5".to_string(),
+            "addx -1".to_string(),
+            "addx -35".to_string(),
+            "addx 1".to_string(),
+            "addx 24".to_string(),
+            "addx -19".to_string(),
+            "addx 1".to_string(),
+            "addx 16".to_string(),
+            "addx -11".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 21".to_string(),
+            "addx -15".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx -3".to_string(),
+            "addx 9".to_string(),
+            "addx 1".to_string(),
+            "addx -3".to_string(),
+            "addx 8".to_string(),
+            "addx 1".to_string(),
+            "addx 5".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx -36".to_string(),
+            "noop".to_string(),
+            "addx 1".to_string(),
+            "addx 7".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 2".to_string(),
+            "addx 6".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 7".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "addx -13".to_string(),
+            "addx 13".to_string(),
+            "addx 7".to_string(),
+            "noop".to_string(),
+            "addx 1".to_string(),
+            "addx -33".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 2".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 8".to_string(),
+            "noop".to_string(),
+            "addx -1".to_string(),
+            "addx 2".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "addx 17".to_string(),
+            "addx -9".to_string(),
+            "addx 1".to_string(),
+            "addx 1".to_string(),
+            "addx -3".to_string(),
+            "addx 11".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx -13".to_string(),
+            "addx -19".to_string(),
+            "addx 1".to_string(),
+            "addx 3".to_string(),
+            "addx 26".to_string(),
+            "addx -30".to_string(),
+            "addx 12".to_string(),
+            "addx -1".to_string(),
+            "addx 3".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx -9".to_string(),
+            "addx 18".to_string(),
+            "addx 1".to_string(),
+            "addx 2".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 9".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx -1".to_string(),
+            "addx 2".to_string(),
+            "addx -37".to_string(),
+            "addx 1".to_string(),
+            "addx 3".to_string(),
+            "noop".to_string(),
+            "addx 15".to_string(),
+            "addx -21".to_string(),
+            "addx 22".to_string(),
+            "addx -6".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "addx 2".to_string(),
+            "addx 1".to_string(),
+            "noop".to_string(),
+            "addx -10".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "addx 20".to_string(),
+            "addx 1".to_string(),
+            "addx 2".to_string(),
+            "addx 2".to_string(),
+            "addx -6".to_string(),
+            "addx -11".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+            "noop".to_string(),
+        ];
+        let expected = vec![
+            "##..##..##..##..##..##..##..##..##..##..",
+            "###...###...###...###...###...###...###.",
+            "####....####....####....####....####....",
+            "#####.....#####.....#####.....#####.....",
+            "######......######......######......####",
+            "#######.......#######.......#######.....",
+        ];
+        for row in &expected {
+            println!("{}", row);
+        }
+        println!("");
+
+        let pixels = part_two(&input);
+        for row in pixels.chunks(40) {
+            println!("{}", row.iter().collect::<String>())
+        }
+
+        assert_eq!(
+            expected.join(""),
+            pixels.iter().collect::<String>().as_str()
+        )
+    }
+}
